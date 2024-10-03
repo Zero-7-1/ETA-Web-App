b5820d5fda0f5fda2e57ad0e18f7d7125e8fcfa4
Zero-7-1 <109064402+Zero-7-1@users.noreply.github.com>
2024-10-03 21:00:57 +0530

    reworking

--- server.js
+++ server.js
@@ -2,15 +2,18 @@ const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors');
 const morgan = require('morgan');
+
+
+dotenv.config({ path: './config/config.env' });
+
 const connectDB = require('./config/db');
 const cors = require('cors');
 
-// env variables from config.env 
-dotenv.config({ path: './config/config.env' });
+
 
 const app = express();
 
-// Connect to the database
+
 connectDB();
 
 // Middleware
@@ -31,10 +34,17 @@ app.use(cors({
 // Routes
 app.use('/api/v1/transactions', require('./routes/transactions'));
 
-// Server settings
+
 const PORT = process.env.PORT || 5001;
 
-// Start server
 app.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold);
 });
+
+
+
+
+
+
+
+
